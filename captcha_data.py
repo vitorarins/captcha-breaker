95e5ea561c9427f20a4ecd72ce7b7e1390670c97
vitorarins <vitorarins@gmail.com>
2016-10-19 21:50:30 -0200

    Fixed problem of captcha_data not bringing number of color channels; Finally got the loss stabilized.

--- captcha_data.py
+++ captcha_data.py
@@ -1,10 +1,11 @@
 import glob, os
 import numpy as np
+from scipy import ndimage, misc
 import cv2
 import random
 
 class OCR_data(object):
-    def __init__(self, num, data_dir, num_classes, batch_size=50, len_code=5, height=60, width=180, resize_height=24, resize_width=88):
+    def __init__(self, num, data_dir, num_classes, batch_size=50, len_code=5, height=60, width=180, resize_height=24, resize_width=88, pixel_depth=255.0, num_channels=1):
         self.num = num
         self.data_dir = data_dir
         self.num_classes = num_classes
@@ -14,6 +15,8 @@ class OCR_data(object):
         self.width = width
         self.resize_height = resize_height
         self.resize_width = resize_width
+        self.pixel_depth = pixel_depth
+        self.num_channels = num_channels
         self.index_in_epoch = 0
         self._imgs = []
         self._labels = []
@@ -21,11 +24,11 @@ class OCR_data(object):
 	    img, label = self.create_captcha(pathAndFilename)
             self._imgs.append(img)
             self._labels.append(label)
-        self._imgs = np.array(self._imgs)
+        self._imgs = np.array(self._imgs).reshape((-1, resize_height, resize_width, num_channels)).astype(np.float32)
         self._labels = np.array(self._labels)
 
     def create_captcha(self, pathAndFilename):
-        img = cv2.imread(pathAndFilename, cv2.IMREAD_COLOR)
+        img = cv2.imread(pathAndFilename, cv2.IMREAD_GRAYSCALE)
         img = cv2.resize(img,(self.resize_width, self.resize_height), interpolation=cv2.INTER_AREA)
         filename, ext = os.path.splitext(os.path.basename(pathAndFilename))
         label = self.create_label(filename)
--- captcha_train.py
+++ captcha_train.py
@@ -1,8 +1,14 @@
 import tensorflow as tf
 import time
+import numpy as np
 from captcha_data import OCR_data
 # Parameters
-learning_rate = 0.001
+initial_learning_rate = 0.01
+decay_steps = 1000
+decay_rate = 0.9
+momentum = 0.9
+l2_beta_param = 3e-4
+dropout_keep_prob = 0.75
 training_iters = 200000
 batch_size = 64
 display_step = 20
@@ -13,7 +19,7 @@ width = 180
 resize_width = 88
 height = 60
 resize_height = 24
-color_channels = 3
+color_channels = 1
 n_chars = 5
 n_classes = 36  # 10+26
 n_training_samples = 180000
@@ -29,6 +35,8 @@ data_test = OCR_data(n_test_samples, './images/test', n_classes)
 # tf Graph input
 x = tf.placeholder(tf.float32, [None, resize_height, resize_width, color_channels])
 y = tf.placeholder(tf.float32, [None, n_chars*n_classes])
+# x_test = tf.placeholder(tf.float32, [n_test_samples, resize_height, resize_width, color_channels])
+# y_test = tf.placeholder(tf.float32, [n_test_samples, n_chars*n_classes])
 
 def print_activations(t):
 	print(t.op.name, t.get_shape().as_list())
@@ -70,30 +78,37 @@ def conv2d(x, W, B, name):
 		return conv
 
 
-def ocr_net(_x, _weights, _biases):
+def ocr_net(_x, _weights, _biases, keep_prob):
 	_x = tf.reshape(_x, shape=[-1, resize_height, resize_width, color_channels])
         tf.image_summary('input', _x, 10)
 
 	conv1 = conv2d(_x, _weights['wc1'], _biases['bc1'], 'conv1')
 	print_activations(conv1)
-	pool1 = max_pool(conv1, k=2, name='pool1')
+        lrn1 = tf.nn.local_response_normalization(conv1)
+	pool1 = max_pool(lrn1, k=2, name='pool1')
 	print_activations(pool1)
 
 	conv2 = conv2d(pool1, _weights['wc2'], _biases['bc2'], 'conv2')
 	print_activations(conv2)
-	pool2 = max_pool(conv2, k=2, name='pool2')
+        lrn2 = tf.nn.local_response_normalization(conv2)
+	pool2 = max_pool(lrn2, k=2, name='pool2')
 	print_activations(pool2)
 
 	conv3 = conv2d(pool2, _weights['wc3'], _biases['bc3'], 'conv3')
 	print_activations(conv3)
-	pool3 = max_pool(conv3, k=2, name='pool3')
+        lrn3 = tf.nn.local_response_normalization(conv3)
+	pool3 = max_pool(lrn3, k=2, name='pool3')
 	print_activations(pool3)
 
 	conv4 = conv2d(pool3, _weights['wc4'], _biases['bc4'], 'conv4')
 	print_activations(conv4)
 
-	conv4_flat = tf.reshape(conv4, [-1, _weights['wd1'].get_shape().as_list()[0]])
-	fc1 = tf.nn.relu(tf.matmul(conv4_flat, _weights['wd1']) + _biases['bd1'], name='fc1')
+        dropout = tf.nn.dropout(conv4, keep_prob)
+
+        shape = dropout.get_shape().as_list()
+	reshaped = tf.reshape(dropout, [-1, _weights['wd1'].get_shape().as_list()[0]])
+
+	fc1 = tf.nn.relu(tf.matmul(reshaped, _weights['wd1']) + _biases['bd1'], name='fc1')
 	print_activations(fc1)
 
 	fc21 = tf.nn.relu(tf.matmul(fc1, _weights['out1']) + _biases['out1'], name='fc21')
@@ -111,21 +126,32 @@ def ocr_net(_x, _weights, _biases):
 	fc25 = tf.nn.relu(tf.matmul(fc1, _weights['out5']) + _biases['out5'], name='fc25')
 	print_activations(fc25)
 
-	out = tf.concat(1, [fc21, fc22, fc23, fc24, fc25], name='out')
-	print_activations(out)
-	return out
+	# out = tf.concat(1, [fc21, fc22, fc23, fc24, fc25], name='out')
+	# print_activations(out)
+	return [fc21, fc22, fc23, fc24, fc25]
 
-def accuracy_func(_pred, _y):
+def accuracy_func(predictions, labels):
+        # predictions = tf.reshape(predictions, shape=[-1, n_chars, n_classes])
+        # labels = tf.reshape(labels, shape=[-1, n_chars, n_classes])
+        # truth_table = np.zeros((predictions.get_shape().as_list()[0],5))
+        # for i in range(5):
+        #         truth_table[:,i] = tf.argmax(predictions[:,i,:], 1) == tf.argmax(labels[:,i,:], 1)
+        # return (100 * np.sum(np.amin(truth_table, axis = 1))/predictions.get_shape().as_list()[0])
         with tf.name_scope('accuracy'):
-	        y = tf.reshape(_y, shape=[-1, n_chars, n_classes])
+	        y = tf.reshape(labels, shape=[-1, n_chars, n_classes])
                 with tf.name_scope('prediction'):
-	                pred = tf.reshape(_pred, shape=[-1, n_chars, n_classes])
+	                pred = tf.reshape(predictions, shape=[-1, n_chars, n_classes])
                 with tf.name_scope('correct_prediction'):
 	                correct_pred = tf.equal(tf.argmax(pred,2), tf.argmax(y,2))
                 with tf.name_scope('accuracy'):
                         accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
                 tf.scalar_summary('accuracy',accuracy)
-	        return accuracy
+	        return accuracy * 100.0
+
+def softmax_joiner(logits):      
+        return tf.transpose(tf.pack([tf.nn.softmax(logits[0]), tf.nn.softmax(logits[1]), \
+                                     tf.nn.softmax(logits[2]), tf.nn.softmax(logits[3]), \
+                                     tf.nn.softmax(logits[4])]), perm = [1,0,2])
 
 weights = {
 	'wc1': weight_variable('wc1',[5, 5, color_channels, 64]),
@@ -155,16 +181,34 @@ biases = {
 
 def train():
         with tf.Session() as sess:
-                pred = ocr_net(x, weights, biases)
-                with tf.name_scope('cross_entropy'):
-                        cross_entropy = tf.nn.softmax_cross_entropy_with_logits(pred,y)
-                        with tf.name_scope('total'):
-                                cost = tf.reduce_mean(cross_entropy)
-                        tf.scalar_summary('cross entropy', cost)
+                logits = ocr_net(x, weights, biases, dropout_keep_prob)
+
+                with tf.name_scope('loss'):
+                        loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits[0],y[:,0:36])) +\
+                               tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits[1],y[:,36:72])) +\
+                               tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits[2],y[:,72:108])) +\
+                               tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits[3],y[:,108:144])) +\
+                               tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits[4],y[:,144:180]))
+
+                        # adding regularizers
+                        regularizers = (tf.nn.l2_loss(weights['wc1']) + tf.nn.l2_loss(biases['bc1']) +
+                                        tf.nn.l2_loss(weights['wc2']) + tf.nn.l2_loss(biases['bc2']) +
+                                        tf.nn.l2_loss(weights['wc3']) + tf.nn.l2_loss(biases['bc3']) +
+                                        tf.nn.l2_loss(weights['wc4']) + tf.nn.l2_loss(biases['bc4'])
+                        )
+                        # Add the regularization term to the loss.
+                        loss += l2_beta_param * regularizers
+                        tf.scalar_summary('loss', loss)
+
+                global_step = tf.Variable(0)
+                learning_rate = tf.train.exponential_decay(initial_learning_rate, global_step, decay_steps, decay_rate)
                 with tf.name_scope('train'):
-                        optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+                        # optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
+                        optimizer = tf.train.MomentumOptimizer(learning_rate, momentum).minimize(loss, global_step=global_step)
+                        # optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
+                pred = softmax_joiner(logits)
                 accuracy = accuracy_func(pred, y)
-                
+
                 init = tf.initialize_all_variables()
                 merged = tf.merge_all_summaries()
                 train_writer = tf.train.SummaryWriter(summaries_dir + '/train', sess.graph)
@@ -173,8 +217,9 @@ def train():
 	        step = 1# Keep training until reach max iterations
 	        while step * batch_size < training_iters:
 		        batch = data_train.next_batch(batch_size)
+                        batch_labels = batch[1]
 		        # Fit training using batch data
-		        smry, _ = sess.run([merged,optimizer], feed_dict={x: batch[0], y: batch[1]})
+		        smry, _, l = sess.run([merged, optimizer, loss], feed_dict={x: batch[0], y: batch_labels})
 		        if step % display_step == 0:
 			        # Calculate batch accuracy
                                 run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
@@ -185,9 +230,7 @@ def train():
                                                         run_metadata=run_metadata)
                                 train_writer.add_run_metadata(run_metadata, 'step%03d' % step)
                                 train_writer.add_summary(summary, step)
-			        # Calculate batch loss
-			        loss = sess.run(cost, feed_dict={x: batch[0], y: batch[1]})
-			        print "Iter " + str(step*batch_size) + ", Minibatch Loss= " + "{:.6f}".format(loss) + ", Training Accuracy= " + "{:.5f}".format(acc)
+			        print "Iter " + str(step*batch_size) + ", Minibatch Loss= " + "{:.6f}".format(l) + ", Training Accuracy= " + "{:.2f}%".format(acc)
                         else:
                                 train_writer.add_summary(smry, step)
 		        step += 1
@@ -202,7 +245,7 @@ def train():
                 test_writer = tf.train.SummaryWriter(summaries_dir + '/test')
 	        test_batch = data_test.next_batch(n_test_samples)
                 summ, acc = sess.run([merged,accuracy], feed_dict={x: test_batch[0], y: test_batch[1]})
-	        print "Testing Accuracy:", acc
+	        print "Testing Accuracy: " + "{:.2f}%".format(acc)
                 test_writer.add_summary(summ,step)
 
 if tf.gfile.Exists(summaries_dir):
